6c5ee3ee9d3182e912ac9b8305e176ed4acc043d
Redas Miliunas <redas.miliunas@outlook.com>
2023-05-05 08:43:58 +0300

    Good start

---
+++ mysite/blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+from datetime import date
+
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(verbose_name='Pavadinimas', max_length=50)
+    text = models.TextField(verbose_name='Tekstas', max_length=5000)
+    author = models.ForeignKey(to=User, verbose_name='Autorius', on_delete=models.CASCADE)
+    date = models.DateTimeField(verbose_name='Data', auto_now_add=True)
+
+class Comment(models.Model):
+    post = models.ForeignKey(to='Post', verbose_name='Straipsnis', on_delete=models.CASCADE)
+    text = models.TextField(verbose_name='Tekstas', max_length=5000)
+    author = models.ForeignKey(to=User, verbose_name='Autorius', on_delete=models.CASCADE)
+    date = models.DateTimeField(verbose_name='Data', auto_now_add=True)
