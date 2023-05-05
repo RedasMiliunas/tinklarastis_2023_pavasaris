5381d1485ffacb842e75eccd8974b7e3b2756ce6
Redas Miliunas <redas.miliunas@outlook.com>
2023-05-05 09:06:59 +0300

    Sutvarkyta, kad veiktu :D

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Post, Comment
+
 
 # Register your models here.
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'date']
+    inlines = [CommentInline]
+
+admin.site.register(Post, PostAdmin)
--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 from datetime import date
+from tinymce.models import HTMLField
 
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(verbose_name='Pavadinimas', max_length=50)
-    text = models.TextField(verbose_name='Tekstas', max_length=5000)
+    text = HTMLField(verbose_name='Tekstas', max_length=5000)
     author = models.ForeignKey(to=User, verbose_name='Autorius', on_delete=models.CASCADE)
     date = models.DateTimeField(verbose_name='Data', auto_now_add=True)
 
